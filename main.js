dd34d7883418e79d11dd4d45ff65c630b8fa0f30
MarshallJavierNossaChanagaCAMPUS <infinitymc777@gmail.com>
2023-03-06 09:19:21 -0500

    EjercicioJS

---
+++ main.js
@@ -0,0 +1,11 @@
+const player = {
+    tradi: ("0", "<60", "60-69", "70-79", "80-89", "90-100"),
+    ponitrage: ("0", "1-2", "3-5", "6-8", "9-11", "12-14"),
+    Letter: ("F", "E", "D", "C", "B", "A"),
+    Rating: (0, 0, 1, 2, 3, 4),
+    proficiency: ("No attempt made", "Lacks all proficiency", "Falls well below proficiency", "Approaches proficiency", "Demostrates proficiency", "Exceeds proficiency")
+  };
+
+player.area = "campus"
+
+console.log(player)
